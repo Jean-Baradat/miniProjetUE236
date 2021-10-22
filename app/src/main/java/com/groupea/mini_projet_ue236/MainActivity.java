e5e2fe1267003bbbc7648649cfb2bed8052116c3
Ovhas <dovakinbaradat@gmail.com>
2021-10-22 13:06:44 +0200

    Activité 1 - Etape 1 - mise en place de la liste des contacts + bouton 100% fonctionnel sur tous types d'écrans

--- app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
@@ -3,6 +3,8 @@ package com.groupea.mini_projet_ue236;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.listContacts);
+        ArrayAdapter<String> tab =
+                new ArrayAdapter<String>(this, R.layout.contact);
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+        tab.add("thomas  06 00 00 00 00");
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+        tab.add("thomas  06 00 00 00 00");
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+        tab.add("thomas  06 00 00 00 00");
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+        tab.add("thomas  06 00 00 00 00");
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+        tab.add("thomas  06 00 00 00 00");
+        tab.add("Jean  06 00 00 00 00");
+        tab.add("pierre  06 00 00 00 00");
+
+        list.setAdapter(tab);
     }
 }
