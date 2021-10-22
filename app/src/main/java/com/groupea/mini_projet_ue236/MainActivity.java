2a5dedd333299f2cc249bb8dad82df25e66d2bbb
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-10-22 18:29:33 +0200

    checkbox done (=étape 2 validée)

--- app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
@@ -18,23 +18,41 @@ public class MainActivity extends AppCompatActivity {
         ListView list = (ListView) findViewById(R.id.listContacts);
         ArrayAdapter<String> tab =
                 new ArrayAdapter<String>(this, R.layout.checkbox);
+        tab.add("jeanne  06 00 00 00 00");
+        tab.add("oskour  06 00 00 00 00");
+        tab.add("goulag  06 00 00 00 00");
+        tab.add("Hortense  06 00 00 00 00");
+        tab.add("tubercule  06 00 00 00 00");
+        tab.add("jeanluc  06 00 00 00 00");
+        tab.add("Philippe  06 00 00 00 00");
+        tab.add("Poutou  06 00 00 00 00");
+        tab.add("eric  06 00 00 00 00");
+        tab.add("antoine  06 00 00 00 00");
+        tab.add("franssoua  06 00 00 00 00");
+        tab.add("hollande  06 00 00 00 00");
         tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
-        tab.add("thomas  06 00 00 00 00");
+        tab.add("Marie  06 00 00 00 00");
+        tab.add("Le Pen  06 00 00 00 00");
+        tab.add("Joseph  06 00 00 00 00");
+        tab.add("Staline  06 00 00 00 00");
+        tab.add("jeanne  06 00 00 00 00");
+        tab.add("oskour  06 00 00 00 00");
+        tab.add("goulag  06 00 00 00 00");
+        tab.add("Hortense  06 00 00 00 00");
+        tab.add("tubercule  06 00 00 00 00");
+        tab.add("jeanluc  06 00 00 00 00");
+        tab.add("Philippe  06 00 00 00 00");
+        tab.add("Poutou  06 00 00 00 00");
+        tab.add("eric  06 00 00 00 00");
+        tab.add("antoine  06 00 00 00 00");
+        tab.add("franssoua  06 00 00 00 00");
+        tab.add("hollande  06 00 00 00 00");
         tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
-        tab.add("thomas  06 00 00 00 00");
-        tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
-        tab.add("thomas  06 00 00 00 00");
-        tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
-        tab.add("thomas  06 00 00 00 00");
-        tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
-        tab.add("thomas  06 00 00 00 00");
-        tab.add("Jean  06 00 00 00 00");
-        tab.add("pierre  06 00 00 00 00");
+        tab.add("Marie  06 00 00 00 00");
+        tab.add("Le Pen  06 00 00 00 00");
+        tab.add("Joseph  06 00 00 00 00");
+        tab.add("Staline  06 00 00 00 00");
+
 
         list.setAdapter(tab);
     }
