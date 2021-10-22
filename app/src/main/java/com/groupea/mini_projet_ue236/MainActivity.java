fb18006dc8897098deee4295ace2a2332f8c490b
Ovhas <dovakinbaradat@gmail.com>
2021-10-22 16:05:29 +0200

    Activité 2 - Etape 1 - mise en place des la deuxième activity avec le bouton annuler pour revenir à l'activité Main et un bouton valider qui affiche un Toast.

--- app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
@@ -2,7 +2,9 @@ package com.groupea.mini_projet_ue236;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -35,5 +37,10 @@ public class MainActivity extends AppCompatActivity {
         tab.add("pierre  06 00 00 00 00");
 
         list.setAdapter(tab);
+    }
+
+    public void boutonNextActiviteContactSelectionne(View v){
+        Intent intent = new Intent(this, ContactSelectionne.class);
+        startActivity(intent);
     }
 }
