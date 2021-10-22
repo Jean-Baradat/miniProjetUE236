497ec944f021439033480a70c078226fa7cf6fe4
Salomé <salome.cliquennois@etu.unilim.fr>
2021-10-22 10:15:40 +0200

    création de la classe Contact

--- app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
@@ -11,4 +11,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public class Contact {
+
+        // variables d'instance
+        private String nom;
+        private String prenom;
+        private int numero;
+
+        // constructeur sans paramètre
+        public Contact() {
+            this.nom = "";
+            this.prenom = "";
+            this.numero = 0000000000;
+        }
+
+        // constructeur avec nom, prenom, numero
+        public Contact(String newNom, String newPrenom, int newNumero) {
+            this.nom = newNom;
+            this.prenom = newPrenom;
+            this.numero = newNumero;
+        }
+
+        // setters
+        public void setNom(String newNom) {
+            this.nom = newNom;
+        }
+
+        public void setPrenom(String newPrenom) {
+            this.prenom = newPrenom;
+        }
+
+        public void setNumero(int newNumero) {
+            this.numero = newNumero;
+        }
+
+        // getters
+        public String getNom() {
+            return this.nom;
+        }
+
+        public String getPrenom() {
+            return this.prenom;
+        }
+
+        public int getNumero() {
+            return this.numero;
+        }
+    }
 }
