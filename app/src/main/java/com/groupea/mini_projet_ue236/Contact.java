a9893cf203a8744a67d34ed6cb5c6e5baf77e82c
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-10-23 16:58:45 +0200

    juste rangement des indentations

--- app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
@@ -2,52 +2,61 @@ package com.groupea.mini_projet_ue236;
 
 import java.io.Serializable;
 
-public class Contact implements Serializable {
+public class Contact implements Serializable
+{
 
     private String contactName;
     private String phoneContact;
 
     private boolean active;
 
-    public Contact(String contactName, String phoneContact)  {
+    public Contact(String contactName, String phoneContact)
+    {
         this.contactName = contactName;
         this.phoneContact = phoneContact;
         this.active= true;
     }
 
-    public Contact(String contactName, String phoneContact, boolean active)  {
+    public Contact(String contactName, String phoneContact, boolean active)
+    {
         this.contactName= contactName;
         this.phoneContact = phoneContact;
         this.active= active;
     }
 
-    public String getPhoneContact() {
+    public String getPhoneContact()
+    {
         return phoneContact;
     }
 
-    public void setphoneContact(String phoneContact) {
+    public void setphoneContact(String phoneContact)
+    {
         this.phoneContact = phoneContact;
     }
 
-    public String getContactName() {
+    public String getContactName()
+    {
         return contactName;
     }
 
-    public void setContactName(String contactName) {
+    public void setContactName(String contactName)
+    {
         this.contactName = contactName;
     }
 
-    public boolean isActive() {
+    public boolean isActive()
+    {
         return active;
     }
 
-    public void setActive(boolean active) {
+    public void setActive(boolean active)
+    {
         this.active = active;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return this.contactName +" ("+ this.phoneContact+")";
     }
-
 }
