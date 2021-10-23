26a05ab7504d5bd8cfafab59e87653e958ef1b68
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-10-23 14:51:56 +0200

    J'AI FINI !!! (bon le toast ne sert à rien mais déjà les checkbox et tout, c'est FONCTIONNEL!!!

---
+++ app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
@@ -0,0 +1,53 @@
+package com.groupea.mini_projet_ue236;
+
+import java.io.Serializable;
+
+public class Contact implements Serializable {
+
+    private String contactName;
+    private String phoneContact;
+
+    private boolean active;
+
+    public Contact(String contactName, String phoneContact)  {
+        this.contactName = contactName;
+        this.phoneContact = phoneContact;
+        this.active= true;
+    }
+
+    public Contact(String contactName, String phoneContact, boolean active)  {
+        this.contactName= contactName;
+        this.phoneContact = phoneContact;
+        this.active= active;
+    }
+
+    public String getPhoneContact() {
+        return phoneContact;
+    }
+
+    public void setphoneContact(String phoneContact) {
+        this.phoneContact = phoneContact;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return this.contactName +" ("+ this.phoneContact+")";
+    }
+
+}
