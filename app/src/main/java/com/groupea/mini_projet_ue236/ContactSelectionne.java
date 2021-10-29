977e357a198329cc3a44d9b705a97e7bb26a2da3
Ovhas <dovakinbaradat@gmail.com>
2021-10-29 13:38:42 +0200

    correction de la longueur du code + some commentaires

--- app/src/main/java/com/groupea/mini_projet_ue236/ContactSelectionne.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/ContactSelectionne.java
@@ -1,6 +1,6 @@
 package com.groupea.mini_projet_ue236;
 
-//diverses bibliothèques
+// diverses bibliothèques
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,43 +12,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class ContactSelectionne extends AppCompatActivity implements Serializable { //AppCompatActivity : supporte les màj de librairies|| Sérialization : les objets deviennent un tableau d'octets
+// AppCompatActivity : supporte les màj de librairies || Sérialization : les objets deviennent un
+// tableau d'octets
+public class ContactSelectionne extends AppCompatActivity implements Serializable {
 
-    String newLine = System.getProperty("line.separator"); //saut de ligne pour le textView listant les contacts sélectionnés
-    public Contact contactChosen; //permettra ensuite d'invoquer le nom du contact sélectionné, au sein du textView
-    final String infoTitre = "Voulez-vous envoyer un message à :"; //simple phrase d'ouverture du sujet
+    // saut de ligne pour le textView listant les contacts sélectionnés
+    String newLine = System.getProperty("line.separator");
+    // permettra ensuite d'invoquer le nom du contact sélectionné, au sein du textView
+    public Contact contactChosen;
+    // simple phrase d'ouverture du sujet
+    final String infoTitre = "Voulez-vous envoyer un message à :";
 
+    // à la création de l'application (= à son ouverture), récupère les données initiales contenues
+    // dans savedInstanceState
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //à la création de l'application (= à son ouverture), récupère les données initiales contenues dans savedInstanceState
+    protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState); //pour que notre code puisse être lu en addition du code existant déjà dans le onCreate()
-        setContentView(R.layout.contact_selectionne); //pose l'interface visuelle depuis les ressources (R) correspondant à contact_selectionne.xml
+        // pour que notre code puisse être lu en addition du code existant déjà dans le onCreate()
+        super.onCreate(savedInstanceState);
+        // pose l'interface visuelle depuis les ressources (R) correspondant à
+        // contact_selectionne.xml
+        setContentView(R.layout.contact_selectionne);
 
-        Bundle infos = getIntent().getExtras(); //récupère les valeurs issues de l'intent stockées dans le bundle
-        final ArrayList<Contact> listSelectedContacts = (ArrayList<Contact>) infos.getSerializable("listSelectedContacts"); //récupère le tableau sous forme d'octets et le "décrypte" grâce à la clé "listSelectedContacts"
+        // récupère les valeurs issues de l'intent stockées dans le bundle
+        Bundle infos = getIntent().getExtras();
+        // récupère le tableau sous forme d'octets et le "décrypte" grâce à la clé
+        // "listSelectedContacts"
+        final ArrayList<Contact> listSelectedContacts =
+                (ArrayList<Contact>) infos.getSerializable("listSelectedContacts");
 
-        TextView textView = findViewById(R.id.textview1); //appel du textView grâce à son id
+        // appel du textView grâce à son id
+        TextView textView = findViewById(R.id.textview1);
 
-        for (int i = 0; i < listSelectedContacts.size() ; i++) { //pour toute la longueur de la liste des contacts sélectionnés (=lit le tableau de haut en bas)
+        // pour toute la longueur de la liste des contacts sélectionnés (=lit le tableau de
+        // haut en bas)
+        for (int i = 0; i < listSelectedContacts.size() ; i++) {
             if (i == 0){
-                textView.append(infoTitre + newLine); //permet d'appeler la phrase d'intro lorsque l'index du nuveau tableau est à 0 (=économise un textView) et rajoute un retour à la ligne
+                // permet d'appeler la phrase d'intro lorsque l'index du nuveau tableau est à 0
+                // (=économise un textView) et rajoute un retour à la ligne
+                textView.append(infoTitre + newLine);
             }
-            contactChosen = listSelectedContacts.get(i); //récupère l'index du contact
-            textView.append(newLine + "• " + contactChosen); //saut de ligne + point de liste + nom du contact à l'index i
+            // récupère l'index du contact et la palce dans contactChosen
+            contactChosen = listSelectedContacts.get(i);
+            // saut de ligne + point de liste + nom du contact à l'index i (contactChosen)
+            textView.append(newLine + "• " + contactChosen);
         }
     }
 
-    public void boutonNextActiviteMainActivity(View v) //retourne à l'activité précédente
+    // retourne à l'activité précédente
+    public void buttonNextActivityMainActivity(View v)
     {
-        //TODO : trouver un moyen de conserver les contacts précédemment cochés afin d'éviter à l'utilisateur de devoir tous les recocher pour une simple erreur ?
+        //TODO : trouver un moyen de conserver les contacts précédemment cochés afin d'éviter à
+        // l'utilisateur de devoir tous les recocher pour une simple erreur ?
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void boutonValider(View view) //Validation du choix des contacts et envoi du message, retourne au début de l'activité précédente
+    // Validation du choix des contacts et envoi du message, retourne à l'activite 1 (MainActivity)
+    public void boutonValider(View view)
     {
-        Toast.makeText(this, "Le message a été envoyé !", Toast.LENGTH_SHORT).show(); //petite bulle en bas de l'écran indiquant que le message a été envoyé aux contacts sélectionnés
-        Intent intent = new Intent(this, MainActivity.class); //fait le lien entre cette activité et l'activité MainActivity
-        startActivity(intent); //lance l'intent décrit
+        // petite bulle en bas de l'écran indiquant que le message a été envoyé aux contacts
+        // sélectionnés
+        Toast.makeText(this, "Le message a été envoyé !", Toast.LENGTH_SHORT).show();
+        // fait le lien entre cette activité et l'activité MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        // lance l'intent décrit
+        startActivity(intent);
     }
 }
