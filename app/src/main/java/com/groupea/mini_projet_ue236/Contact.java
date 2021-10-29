0bbdc899a23513b0ece41516dd54bdd4cd98c88e
Ovhas <dovakinbaradat@gmail.com>
2021-10-29 13:38:42 +0200

    correction de la longueur du code + some commentaires

--- app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
@@ -1,19 +1,24 @@
 package com.groupea.mini_projet_ue236;
 
-//diverses bibliothèques
+// diverses bibliothèques
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.Serializable;
 
-public class Contact extends AppCompatActivity implements Serializable //AppCompatActivity : supporte les màj de librairies|| Sérialization : les objets deviennent un tableau d'octets
+// AppCompatActivity : supporte les màj de librairies || Sérialization : les objets deviennent un
+// tableau d'octets
+public class Contact extends AppCompatActivity implements Serializable
 {
-    //Variables d'instance
-    private String contactName; //nom du contact
-    private String phoneContact; //numéro de t&léphone du contact
+    // Variables d'instance
+    // nom du contact
+    private String contactName;
+    // numéro de telephone du contact
+    private String phoneContact;
 
-    private boolean active; //est lié d'une façon ou d'une autre au currentCheck.isActive() du MainActivity
+    // lié d'une façon ou d'une autre au currentCheck.isActive() du MainActivity
+    private boolean active;
 
-    //Constructeur avec seulement le nom et le numéro de téléphone en paramètres
+    // Constructeur avec seulement le nom et le numéro de téléphone en paramètres
     public Contact(String contactName, String phoneContact)
     {
         this.contactName = contactName;
@@ -21,7 +26,7 @@ public class Contact extends AppCompatActivity implements Serializable //AppComp
         this.active= true; //boolean true
     }
 
-    //Constructeur avec le nom, le numéro de télélphone, et l'état du boolean en parapètres
+    // Constructeur avec le nom, le numéro de télélphone, et l'état du boolean en parapètres
     public Contact(String contactName, String phoneContact, boolean active)
     {
         this.contactName= contactName;
@@ -29,42 +34,46 @@ public class Contact extends AppCompatActivity implements Serializable //AppComp
         this.active= active; //boolean true
     }
 
-    //Méthode récupère le numéro de téléphone du contact
+    // Méthode récupère le numéro de téléphone du contact
     public String getPhoneContact()
     {
         return phoneContact;
     }
 
-    //Méthode qui définit le numéro de téléphone du contact
+    // Méthode qui définit le numéro de téléphone du contact
     public void setphoneContact(String phoneContact)
     {
         this.phoneContact = phoneContact;
     }
 
-    //Méthode qui récupère le nom du contact
+    // Méthode qui récupère le nom du contact
     public String getContactName()
     {
         return contactName;
     }
 
-    //Méthode qui définit le nom du contact
+    // Méthode qui définit le nom du contact
     public void setContactName(String contactName)
     {
         this.contactName = contactName;
     }
 
+    // Méthode qui récupère le parametre active
     public boolean isActive()
     {
         return active;
     }
 
+    // Méthode pour définir le parametre active avec un boolean (true/false)
     public void setActive(boolean active)
     {
         this.active = active;
     }
 
+    // donne la structure d'affichage de chaque objet contact.
     @Override
-    public String toString() //donne la structure d'affichage de chaque objet contact au sein de la listView située dans la mainactivity
+    // TODO voir pour peut-etre modif le nom de cette méthode toString()
+    public String toString()
     {
         return this.contactName +" ("+ this.phoneContact+")";
     }
--- app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/MainActivity.java
@@ -1,6 +1,6 @@
 package com.groupea.mini_projet_ue236;
 
-//Import de diverses bibliothèques
+// Import de diverses bibliothèques
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
@@ -15,91 +15,145 @@ import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements Serializable { //AppCompatActivity : supporte les màj de librairies|| Sérialization : les objets deviennent un tableau d'octets
-
+// AppCompatActivity : supporte les màj de librairies || Sérialization : les objets deviennent un
+// tableau d'octets
+public class MainActivity extends AppCompatActivity implements Serializable {
 
     private ListView listView;
     private ArrayList<Contact> listeDesContacts;
     private ArrayList<Contact> listeDesContactsSelectionnes;
 
+    // à la création de l'application (= à son ouverture), récupère les données initiales contenues
+    // dans savedInstanceState
     @Override
-    protected void onCreate(Bundle savedInstanceState) //à la création de l'application (= à son ouverture), récupère les données initiales contenues dans savedInstanceState
+    protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState); //pour que notre code puisse être lu en addition du code existant déjà dans le onCreate()
-        setContentView(R.layout.activity_main); //pose l'interface visuelle depuis les ressources (R) correspondant à activity_main.xml
+        // pour que notre code puisse être lu en addition du code existant déjà dans le onCreate()
+        super.onCreate(savedInstanceState);
+        // pose l'interface visuelle depuis les ressources (R) correspondant à activity_main.xml
+        setContentView(R.layout.activity_main);
 
-        CheckBox checkBoxCheckAll = findViewById(R.id.checkBoxCheckAll); //appel de la checkbox qui coche toutes les autres checkbox de la listView grâce à son ID "checkBoxCheckAll"
+        // appel de la checkbox qui coche toutes les autres checkbox de la listView grâce à son ID
+        // "checkBoxCheckAll"
+        CheckBox checkBoxCheckAll = findViewById(R.id.checkBoxCheckAll);
 
-        this.listView = findViewById(R.id.listView); //appel de la listView depuis son ID listView
+        // appel de la listView depuis son ID listView
+        this.listView = findViewById(R.id.listView);
 
-        this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE); //permet aux checkbox contenues dans la listView de fonctionner (au moins visuellement)
+        // défini le mode de choix des checkbox en MULTIPLE
+        this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
 
-        //Ceci concerne le comportement des checkbox au sein de la listView
-        this.listView.setOnItemClickListener((parent, view, position, id) -> { //écoute du clic sur la listView, avec en paramètre l'adapter où a lieu le clic, la view dans l'adapterView qui a été cliquée (l'adapter s'en occupe), la position de la vue dans l'adapter, et l'id de l'item qui a été cliqué
-            //ce qu'il se passe lorsque l'on a cliqué sur une checkbox de la listView
+        // Ceci concerne le comportement des checkbox au sein de la listView.
+        /* écoute du clic sur la listView, avec en paramètre l'adapter où a lieu le clic, la
+        view dans l'adapterView qui a été cliquée (l'adapter s'en occupe), la position de la
+        vue dans l'adapter, et l'id de l'item qui a été cliqué */
+        this.listView.setOnItemClickListener((parent, view, position, id) -> {
 
-            CheckedTextView v = (CheckedTextView) view; //
-            boolean currentCheck = v.isChecked(); //permettra de signifier si la vue correspondant à la checkbox de la listView est cochée ou non grâce à true ou false
-            Contact contact = (Contact) listView.getItemAtPosition(position); //récupère la position de la ckeckbox qui vient d'être cochée au sein de la listView qui contient les objets contacts
-            contact.setActive(!currentCheck); //permet de changer l'état de la checkbox lorsque l'on clique dessus en tenant compte de son état précédent (true devient false, false devient true)
+            // ce qu'il se passe lorsque l'on a cliqué sur une checkbox de la listView
+            CheckedTextView v = (CheckedTextView) view;
+            // permettra de signifier si la vue correspondant à la checkbox de la listView est
+            // cochée ou non grâce à true ou false
+            boolean currentCheck = v.isChecked();
+            // récupère la position de la ckeckbox qui vient d'être cochée au sein de la listView
+            // qui contient les objets contacts
+            Contact contact = (Contact) listView.getItemAtPosition(position);
+            // permet de changer l'état de la checkbox lorsque l'on clique dessus en tenant compte
+            // de son état précédent (true devient false, false devient true)
+            contact.setActive(!currentCheck);
         });
 
 
-        //Ceci concerne la checkbox qui coche toutes les autres présentes dans la listView /!\ N'est pas dans la listView !
+        // Ceci concerne la checkbox qui coche toutes les autres présentes dans la listView /!\
+        // N'est pas dans la listView !
         checkBoxCheckAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) // Appelé lorsque l'état coché d'un bouton composé a changé, ici donc sur notre checkbox unique qui sélectionne toutes les autres.
+            // Appelé lorsque l'état coché d'un bouton composé a changé, ici donc sur notre checkbox
+            // unique qui sélectionne toutes les autres.
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
             {
-                if (checkBoxCheckAll.isChecked()) //si elle est cochée
+                // si elle est cochée
+                if (checkBoxCheckAll.isChecked())
                 {
-                    for (int i = 0 ; i < listeDesContacts.size() ; i++) //pour toutes les checkbox le long de la listView
+                    // pour toutes les checkbox le long de la listView
+                    for (int i = 0 ; i < listeDesContacts.size() ; i++)
                     {
-                        listView.setItemChecked(i, true); //toutes les checkbox de la listView sont cochées ensemble
+                        // toutes les checkbox de la listView sont cochées ensemble
+                        listView.setItemChecked(i, true);
                     }
                 }
-                else //si on la décoche manuellement
+                // si on la décoche manuellement
+                else
                 {
-                    for (int i = 0 ; i < listeDesContacts.size() ; i++) //pour toutes les checkbox le long de la listView
+                    // pour toutes les checkbox le long de la listView
+                    for (int i = 0 ; i < listeDesContacts.size() ; i++)
                     {
-                        listView.setItemChecked(i, false); //toutes les checkbox de la listView sont décochées ensemble (uniquement lorsque nous appuyons sur la checkbox qui les coche toutes)
-                        //TODO : ajouter le truc qui permet au checkBoxCheckAll d être décoché quand un contact est désélectionné
-                        //TODO BIS : tenter de faire comme le currentCheck
+                        // toutes les checkbox de la listView sont décochées ensemble (uniquement
+                        // lorsque nous appuyons sur la checkbox qui les coche toutes)
+                        listView.setItemChecked(i, false);
+                        // TODO : ajouter le truc qui permet au checkBoxCheckAll d être décoché
+                        //  quand un contact est désélectionné
+                        // TODO BIS : tenter de faire comme le currentCheck
                     }
                 }
             }
         });
 
-        this.initListViewData(); //Permet de faire apparaitre les données en suivant les paramètres qui leurs sont attribués
+        // Execution de la methode initListViewData : mise en place des elements de la liste
+        // ListView
+        this.initListViewData();
     }
 
-    private void initListViewData() { //Créé les objets contacts, les ajoute dans un arrayList, et contient la gestion du coche solo
+    // Créé les objets contacts, ajoutes dans un arrayList, gestion du coche solo, creation de
+    // l'adapter
+    private void initListViewData() {
 
-        //Création des objets Contact
-        Contact Jean = new Contact("Jean Baradat", "0033601030507", false);
-        Contact Lucie = new Contact ("Lucie Dumas", "0033600000000", false);
-        Contact Salome = new Contact ("Salomé Cliquennois", "0033601030507", false);
-        Contact Henri = new Contact ("Henri Michaud", "0033601020304", false);
-        Contact Valentine = new Contact ("Valentine Maillard", "0033601020304", false);
-        Contact Adelaide = new Contact ("Adélaïde Machon", "0033601020304", false);
-        Contact Mathilde = new Contact ("Mathilde Titi", "0033601020304", false);
-        Contact Emma = new Contact ("Emma Simpson", "0033601020304", false);
-        Contact Dave = new Contact ("Dave Strider", "0033601020304", false);
-        Contact Lou = new Contact ("Lou Pépèle", "0033601020304", false);
-        Contact Yannis = new Contact ("Yannis Kuro", "0033601020304", false);
-        Contact Achille = new Contact ("Achille Talon", "0033601020304", false);
-        Contact Gregory = new Contact ("Grégory Flotti", "0033601020304", false);
-        Contact Winnie = new Contact ("Winnie L'Ourson", "0033611223344", false);
-        Contact Gerard = new Contact ("Gérard Menvussa", "0033611223344", false);
-        Contact Jean_Marie = new Contact ("Jean-Marie Saint-Joseph", "0033601020304", false);
-        Contact Zinedine = new Contact ("Zinedine Zidane", "003311335577", false);
-        Contact Germaine = new Contact ("Germaine Langlisse", "0033601020304", false);
-        Contact Titouan = new Contact ("Titouan Gaming", "0033601020304", false);
-        Contact Denis = new Contact ("Denis Brogniart", "0033600000000", false);
-        Contact Didier = new Contact ("Didier Deschamps", "0033600000001", false);
+        // Création des objets Contact
+        Contact Jean = new Contact(
+                "Jean Baradat", "0033601030507",false);
+        Contact Lucie = new Contact (
+                "Lucie Dumas", "0033600000000", false);
+        Contact Salome = new Contact (
+                "Salomé Cliquennois", "0033601030507", false);
+        Contact Henri = new Contact (
+                "Henri Michaud", "0033601020304", false);
+        Contact Valentine = new Contact (
+                "Valentine Maillard", "0033601020304", false);
+        Contact Adelaide = new Contact (
+                "Adélaïde Machon", "0033601020304", false);
+        Contact Mathilde = new Contact (
+                "Mathilde Titi", "0033601020304", false);
+        Contact Emma = new Contact (
+                "Emma Simpson", "0033601020304", false);
+        Contact Dave = new Contact (
+                "Dave Strider", "0033601020304", false);
+        Contact Lou = new Contact (
+                "Lou Pépèle", "0033601020304", false);
+        Contact Yannis = new Contact (
+                "Yannis Kuro", "0033601020304", false);
+        Contact Achille = new Contact (
+                "Achille Talon", "0033601020304", false);
+        Contact Gregory = new Contact (
+                "Grégory Flotti", "0033601020304", false);
+        Contact Winnie = new Contact (
+                "Winnie L'Ourson", "0033611223344", false);
+        Contact Gerard = new Contact (
+                "Gérard Menvussa", "0033611223344", false);
+        Contact Jean_Marie = new Contact (
+                "Jean-Marie Saint-Joseph", "0033601020304", false);
+        Contact Zinedine = new Contact (
+                "Zinedine Zidane", "003311335577", false);
+        Contact Germaine = new Contact (
+                "Germaine Langlisse", "0033601020304", false);
+        Contact Titouan = new Contact (
+                "Titouan Gaming", "0033601020304", false);
+        Contact Denis = new Contact (
+                "Denis Brogniart", "0033600000000", false);
+        Contact Didier = new Contact (
+                "Didier Deschamps", "0033600000001", false);
 
-        //Ajout de ceux-ci dans un arrayList
+        // Ajout de ceux-ci dans un arrayList
         this.listeDesContacts = new ArrayList<Contact>();
         listeDesContacts.add(Jean);
         listeDesContacts.add(Lucie);
@@ -123,41 +177,58 @@ public class MainActivity extends AppCompatActivity implements Serializable { //
         listeDesContacts.add(Denis);
         listeDesContacts.add(Didier);
 
-        //ArrayAdapter qui permet d'utiliser le layout par défaut spécifiquement prévu pour les sélections multiples et de l'appliquer aux checkbox de notre listView
-        ArrayAdapter<Contact> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, getListeDesContacts());
-        this.listView.setAdapter(arrayAdapter); //Met en place l'arrayAdapter
+        // ArrayAdapter qui permet d'utiliser le layout par défaut spécifiquement prévu pour les
+        // sélections multiples et de l'appliquer aux checkbox de notre listView
+        ArrayAdapter<Contact> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, getListeDesContacts());
+        // Met en place l'arrayAdapter
+        this.listView.setAdapter(arrayAdapter);
 
+        // pour toutes les checkbox le long de la listView
+        for(int i = 0 ; i < listeDesContacts.size() ; i++ ) {
 
-        for(int i = 0 ; i < listeDesContacts.size() ; i++ ) { //pour toutes les checkbox le long de la listView
-
-            this.listView.setItemChecked(i, listeDesContacts.get(i).isActive()); //lorsque la checkbox dans la listView est cochée, passe en true, et récupère son index pour ne pas l'oublier au scroll
+            // lorsque la checkbox dans la listView est cochée, passe en true, et récupère son index
+            // pour ne pas l'oublier au scroll
+            this.listView.setItemChecked(i, listeDesContacts.get(i).isActive());
         }
     }
 
-    public ArrayList<Contact> getListeDesContacts() { //récupère et retourne la liste des contacts
+    // retourne la liste des contacts
+    public ArrayList<Contact> getListeDesContacts() {
         return listeDesContacts;
     }
 
-    public ArrayList<Contact> getListeDesContactsSelectionnes() { //récupère et retourne la liste des contacts sélectionnés
+    // retourne la liste des contacts sélectionnés
+    public ArrayList<Contact> getListeDesContactsSelectionnes() {
         return listeDesContactsSelectionnes;
     }
 
-    public void printSelectedItems(View v){ //Créé une nouvelle arrayList contenant les contacts choisis précédemment (merci aux index) afin de pouvoir les transporter plus facilement à la prochaine activité
-                                            //TODO : y trouver un meilleur nom mais là j'ai la tête dans le baba déso pas déso
+    // Créer une nouvelle arrayList contenant les contacts choisis précédemment (merci aux index)
+    // afin de pouvoir les transporter plus facilement à la prochaine activité
+    public void buttonNextActivityContactSelectionne(View v){
 
-        this.listeDesContactsSelectionnes = new ArrayList<Contact>(); //création de l'arrayList
-        SparseBooleanArray sp = listView.getCheckedItemPositions(); //récupère les positions des checkbox cochées au sein de la listView
+        // création de l'arrayList
+        this.listeDesContactsSelectionnes = new ArrayList<Contact>();
+        // récupère les positions des checkbox cochées au sein de la listView
+        SparseBooleanArray sp = listView.getCheckedItemPositions();
 
-        for(int i = 0 ; i < getListeDesContacts().size() ; i++) { //pour toute la liste de contacts
+        // pour toute la liste de contacts
+        for(int i = 0 ; i < getListeDesContacts().size() ; i++) {
 
-            if (sp.valueAt(i) == true) { //si une checkbox cochée est détectée peu importe où dans la listView
+            // si une checkbox cochée est détectée peu importe où dans la listView
+            if (sp.valueAt(i) == true) {
 
-                this.listeDesContactsSelectionnes.add(listeDesContacts.get(i)); //son index est ajouté dans le nouvel array qui sera transporté à l'activité 2
+                // son index est ajouté dans le nouvel array qui sera transporté à l'activité 2
+                this.listeDesContactsSelectionnes.add(listeDesContacts.get(i));
             }
         }
 
-        Intent intent = new Intent(this, ContactSelectionne.class); //fait le lien entre cette activité et l'activité ContactSelectionne
-        intent.putExtra("listSelectedContacts", listeDesContactsSelectionnes); //récupère la liste des contacts sélectionnés, avec ce qui servira de clé sur l'autre activité : "listSelectedContacts"
-        startActivity(intent); //lance l'intent décrit
+        // fait le lien entre cette activité et l'activité ContactSelectionne
+        Intent intent = new Intent(this, ContactSelectionne.class);
+        // récupère la liste des contacts sélectionnés, avec ce qui servira de clé sur l'autre
+        // activité : "listSelectedContacts"
+        intent.putExtra("listSelectedContacts", listeDesContactsSelectionnes);
+        // lance l'intent décrit
+        startActivity(intent);
     }
 }
