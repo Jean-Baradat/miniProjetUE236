7d35d23c9155b23788083a4730a562cfe8c763c1
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-10-29 03:45:19 +0200

    ajout du bouton sélectionner tout presque fonctionnel, et commentaire des codes java

--- app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/Contact.java
@@ -1,45 +1,53 @@
 package com.groupea.mini_projet_ue236;
 
+//diverses bibliothèques
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.Serializable;
 
-public class Contact extends AppCompatActivity implements Serializable
+public class Contact extends AppCompatActivity implements Serializable //AppCompatActivity : supporte les màj de librairies|| Sérialization : les objets deviennent un tableau d'octets
 {
-    private String contactName;
-    private String phoneContact;
+    //Variables d'instance
+    private String contactName; //nom du contact
+    private String phoneContact; //numéro de t&léphone du contact
 
-    private boolean active;
+    private boolean active; //est lié d'une façon ou d'une autre au currentCheck.isActive() du MainActivity
 
+    //Constructeur avec seulement le nom et le numéro de téléphone en paramètres
     public Contact(String contactName, String phoneContact)
     {
         this.contactName = contactName;
         this.phoneContact = phoneContact;
-        this.active= true;
+        this.active= true; //boolean true
     }
 
+    //Constructeur avec le nom, le numéro de télélphone, et l'état du boolean en parapètres
     public Contact(String contactName, String phoneContact, boolean active)
     {
         this.contactName= contactName;
         this.phoneContact = phoneContact;
-        this.active= active;
+        this.active= active; //boolean true
     }
 
+    //Méthode récupère le numéro de téléphone du contact
     public String getPhoneContact()
     {
         return phoneContact;
     }
 
+    //Méthode qui définit le numéro de téléphone du contact
     public void setphoneContact(String phoneContact)
     {
         this.phoneContact = phoneContact;
     }
 
+    //Méthode qui récupère le nom du contact
     public String getContactName()
     {
         return contactName;
     }
 
+    //Méthode qui définit le nom du contact
     public void setContactName(String contactName)
     {
         this.contactName = contactName;
@@ -56,7 +64,7 @@ public class Contact extends AppCompatActivity implements Serializable
     }
 
     @Override
-    public String toString()
+    public String toString() //donne la structure d'affichage de chaque objet contact au sein de la listView située dans la mainactivity
     {
         return this.contactName +" ("+ this.phoneContact+")";
     }
--- app/src/main/java/com/groupea/mini_projet_ue236/ContactSelectionne.java
+++ app/src/main/java/com/groupea/mini_projet_ue236/ContactSelectionne.java
@@ -1,5 +1,6 @@
 package com.groupea.mini_projet_ue236;
 
+//diverses bibliothèques
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,42 +12,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class ContactSelectionne extends AppCompatActivity implements Serializable {
+public class ContactSelectionne extends AppCompatActivity implements Serializable { //AppCompatActivity : supporte les màj de librairies|| Sérialization : les objets deviennent un tableau d'octets
 
-    String newLine = System.getProperty("line.separator");
-    public Contact contactChosen;
-    final String infoTitre = "Voulez-vous envoyer un message à :";
+    String newLine = System.getProperty("line.separator"); //saut de ligne pour le textView listant les contacts sélectionnés
+    public Contact contactChosen; //permettra ensuite d'invoquer le nom du contact sélectionné, au sein du textView
+    final String infoTitre = "Voulez-vous envoyer un message à :"; //simple phrase d'ouverture du sujet
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //à la création de l'application (= à son ouverture), récupère les données initiales contenues dans savedInstanceState
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_selectionne);
+        super.onCreate(savedInstanceState); //pour que notre code puisse être lu en addition du code existant déjà dans le onCreate()
+        setContentView(R.layout.contact_selectionne); //pose l'interface visuelle depuis les ressources (R) correspondant à contact_selectionne.xml
 
-        Bundle infos = getIntent().getExtras();
-        final ArrayList<Contact> listSelectedContacts = (ArrayList<Contact>) infos.getSerializable("listSelectedContacts");
+        Bundle infos = getIntent().getExtras(); //récupère les valeurs issues de l'intent stockées dans le bundle
+        final ArrayList<Contact> listSelectedContacts = (ArrayList<Contact>) infos.getSerializable("listSelectedContacts"); //récupère le tableau sous forme d'octets et le "décrypte" grâce à la clé "listSelectedContacts"
 
-        TextView textView = findViewById(R.id.textview1);
+        TextView textView = findViewById(R.id.textview1); //appel du textView grâce à son id
 
-        for (int i = 0; i < listSelectedContacts.size() ; i++) {
+        for (int i = 0; i < listSelectedContacts.size() ; i++) { //pour toute la longueur de la liste des contacts sélectionnés (=lit le tableau de haut en bas)
             if (i == 0){
-                textView.append(infoTitre + newLine);
+                textView.append(infoTitre + newLine); //permet d'appeler la phrase d'intro lorsque l'index du nuveau tableau est à 0 (=économise un textView) et rajoute un retour à la ligne
             }
-            contactChosen = listSelectedContacts.get(i);
-            textView.append(newLine + "• " + contactChosen);
+            contactChosen = listSelectedContacts.get(i); //récupère l'index du contact
+            textView.append(newLine + "• " + contactChosen); //saut de ligne + point de liste + nom du contact à l'index i
         }
     }
 
-    public void boutonNextActiviteMainActivity(View v)
+    public void boutonNextActiviteMainActivity(View v) //retourne à l'activité précédente
     {
+        //TODO : trouver un moyen de conserver les contacts précédemment cochés afin d'éviter à l'utilisateur de devoir tous les recocher pour une simple erreur ?
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void boutonValider(View view)
+    public void boutonValider(View view) //Validation du choix des contacts et envoi du message, retourne au début de l'activité précédente
     {
-        Toast.makeText(this, "Le message a été envoyé !", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Toast.makeText(this, "Le message a été envoyé !", Toast.LENGTH_SHORT).show(); //petite bulle en bas de l'écran indiquant que le message a été envoyé aux contacts sélectionnés
+        Intent intent = new Intent(this, MainActivity.class); //fait le lien entre cette activité et l'activité MainActivity
+        startActivity(intent); //lance l'intent décrit
     }
 }
