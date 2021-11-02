a007e1079ccc8f9e100e4e8e00d1c6751408f74c
Ovhas <dovakinbaradat@gmail.com>
2021-11-02 18:04:20 +0100

    étape 9 et 10 : - un texte demandant à l'utilisateur de sélectionner un message - des textes prédéfinis - comprenant chacun une chaîne de caractères de la forme "<prénom>" - responsive - Notifier de manière visuelle à l'utilisateur qu'il a bien sélectionné un texte - en dessous de votre contenu scrollable un bouton d'envoi du message (existe déja : bouton valider)

---
+++ app/src/main/java/com/groupea/mini_projet_ue236/ListViewMessageType.java
@@ -0,0 +1,14 @@
+package com.groupea.mini_projet_ue236;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListViewMessageType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view_message_type);
+    }
+}
